2fe4ef2c0ca57768bf5ee890e481550e50dce700
URIAN VIERA <urian1213viera@gmail.com>
2023-10-30 18:50:35 -0500

    ajustando detalles

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ###### Para este ejemplo práctico, aprenderás a consumir una API utilizando Node.js y Express, ademas el motor de plantillas EJS. Primero, asegúrate de tener instalado Node.js y Express en tu proyecto. Luego, instala EJS como motor de plantilla html y axios para ejecutar solicitud HTTP
 
-`npm install express axios ejs
-`
+# Instalar
+
+    npm install express axios ejs
+
 
 ![](https://raw.githubusercontent.com/urian121/imagenes-proyectos-github/master/resultado-api-nodejs.PNG)
 
@@ -16,8 +18,11 @@
 
     node --watch app.js ---->Opcion 1
     node app.js    -----> Opcion 2
+    npm run dev  ---> Opcion 3
+
 
 ### Notas
-#### La propiedad   "type": "module", en el archivo packege.json indica que estamos usando el sistema de modulos ECMAScript(ESM).
-#### Con el fin de  las palabras claves 'import' y 'export' para importar y exportar modulos respectivamente.
-#### Cuando usamos (ESM) algunas caracteriscticas de nodejs como require() y module.exports no estan disponibles, en su lugar estan import y export.
+
+    La propiedad   "type": "module", en el archivo packege.json indica que estamos usando el sistema de modulos ECMAScript(ESM).
+    Con el fin de  las palabras claves 'import' y 'export' para importar y exportar modulos respectivamente.
+    Cuando usamos (ESM) algunas caracteriscticas de nodejs como require() y module.exports no estan disponibles, en su lugar estan import y export.
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import axios from "axios";
+import express from "express"; //Framework express
+import cors from "cors"; //Para permitir o restringir las solicitudes HTTP realizadas desde un origen
+import axios from "axios"; //Para hacer solicitudes HTTP
 
 // Creando una nueva aplicación Express.
 const app = express();
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json()); // Para analizar JSON en el cuerpo de las solicitudes
 app.use(express.urlencoded({ extended: true })); // Para analizar datos de formulario en el cuerpo de las solicitudes
 
+// Para servir archivos estáticos.
 app.use("/public", express.static("public"));
 
 /**
@@ -18,14 +19,10 @@ app.use("/public", express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-const PORT = process.env.PORT || 3001;
-
 /**
- * En este caso, utilizamos la palabra clave async antes de la función de manejo de la ruta ((req, res) => { ... })
- * para indicar que queremos usar await dentro de la función. Luego, utilizamos await antes de la llamada a axios.get(url)
+ * Usamos async para indicar que queremos usar await dentro de la función.
+ * Luego, utilizamos await antes de la llamada a axios.get(url)
  * para esperar a que la solicitud a la API se complete y obtengamos la respuesta.
- * Si la solicitud es exitosa, asignamos los datos a la variable listaJugadores y renderizamos la plantilla index.ejs.
- * Si ocurre algún error, lo capturamos y lo mostramos en la consola.
  */
 const url_API =
   "https://projects.urianviera.com/api_node/api_jugadores_nba.json";
@@ -33,12 +30,14 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get(url_API);
     const listaJugadores = response.data.listaJugadores;
+
     res.render("index", { listaJugadores });
   } catch (error) {
     console.error(error);
   }
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Servidor en http://127.0.0.1:${PORT}`);
 });
